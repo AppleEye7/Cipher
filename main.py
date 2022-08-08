4df22405113fee0df8c047b37291b1443e56e6e3
kemberr <109934204+kemberr@users.noreply.github.com>
2022-08-08 15:10:44 +1200

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-#functions 
+#Caesar shift
+import string
+shift = 2
+message = "hello there"
+class Caesar_shift(shift, message):
+
+    def encode(shift, message):
+        alphabet = string.ascii_lowercase
+        shifted = alphabet[shift:] + alphabet[:shift]
+        table = str.maketrans(alphabet, shifted)
+        encoded = message.translate(table)
+        return(encoded)
+
+
+cipher = Caesar_shift(shift)
+print(cipher.encode(message))
