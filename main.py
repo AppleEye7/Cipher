2bfacb959381924cb2100e0ece7f1d932baa71ce
kemberr <109934204+kemberr@users.noreply.github.com>
2022-08-10 09:40:14 +1200

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-#Caesar shift
-import string
-shift = 2
-message = "hello there persom"
 
-choice = int(input("Welcome to cipher\nWhat do you want to do?\n(please chose corresponding number)\n1.Encode\n2.Decode\n3.Quit\n"))
-if choice == 1:
-    alphabet = string.ascii_lowercase
-    shifted = alphabet[shift:] + alphabet[:shift]
-    table = str.maketrans(alphabet, shifted)
-    encoded = message.translate(table)
-    print(encoded)
-else:
-    print("___")
+while True:
+    choicenum = int(input("Welcome to cipher\nWhat do you want to do?\n"
+                          "(please chose corresponding number)\n1.Encode\n2.Decode\n3.Quit\n"))
+    if choicenum == 1:
+        choice = "encode"
+    elif choicenum == 2:
+        choice = "decode"
+    elif choicenum == 3:
+        break
+    ciphernum = int(input("Chose the cipher you would like to use to {} your message\n"
+                          "(please chose corresponding number)\n1.Caesar Shift\n2.Pigpen\n3.Phonetic Alphabet\n4.Quit\n5.Home\n".format(choice)))
+    if ciphernum == 1:
+        cipher = "Caesar Shift"
+    elif ciphernum == 2:
+        cipher = "Pigpen"
+    elif ciphernum == 3:
+        cipher = "Phonetic Alphabet"
+    elif ciphernum == 4:
+        break
+    elif ciphernum == 5:
+        continue
+    message = int(input("You chose {} to {} your message\nEnter message without using numbers (ei one,three)\n"
+                        "(Chose corresponding number if needed)\n1.Quit \n2.Home\n".format(cipher choice)))
+    if message =
