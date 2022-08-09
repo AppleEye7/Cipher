9523172c47a54c8aa40c4c0da2751deef0d24cce
kemberr <109934204+kemberr@users.noreply.github.com>
2022-08-10 09:00:27 +1200

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import string
 shift = 2
 message = "hello there persom"
 
-choice = int(input("1.encode\n2.decode\n"))
+choice = int(input("Welcome to cipher\nWhat do you want to do?\n(please chose corresponding number)\n1.Encode\n2.Decode\n3.Quit\n"))
 if choice == 1:
     alphabet = string.ascii_lowercase
     shifted = alphabet[shift:] + alphabet[:shift]
@@ -11,4 +11,4 @@ if choice == 1:
     encoded = message.translate(table)
     print(encoded)
 else:
-    print("hi")
+    print("___")
