91537dad0573edcf44dd27495528a53cfe13ddc6
kemberr <109934204+kemberr@users.noreply.github.com>
2022-08-11 13:24:34 +1200

    Add files via upload

--- Ceaser Shift.py
+++ Ceaser Shift.py
@@ -1,10 +1,25 @@
 #Caesar shift
 import string
 shift = 2
-message = "hello there person"
+message = "jgnnq vjgtg rgtuqp"
 
-alphabet = string.ascii_lowercase
-shifted = alphabet[shift:] + alphabet[:shift]
-table = str.maketrans(alphabet, shifted)
-encoded = message.translate(table)
-print(encoded)
+def Caear_shift_de(shift,message):
+    alphabet = string.ascii_lowercase
+    shifted = alphabet[shift:] + alphabet[:shift]
+    table = str.maketrans(alphabet,shifted)
+    decoded = message.translate(table)
+    print(decoded)
+
+def Caear_shift_en(shift,message):
+    alphabet = string.ascii_lowercase
+    shifted = alphabet[shift:] + alphabet[:shift]
+    table = str.maketrans(shifted,alphabet)
+    encoded = message.translate(table)
+    print(encoded)
+
+choicenum = 1
+ciphernum = 1
+if choicenum == 1 and ciphernum == 1:
+    Caear_shift_en(shift,message)
+elif choicenum == 2 and ciphernum == 1:
+    Caear_shift_de(shift,message)
