dd884a0e34e78ff93a9daf60203163af7ac42c12
kemberr <109934204+kemberr@users.noreply.github.com>
2022-08-11 13:32:33 +1200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import string
+def Caear_Shift_de(shift,message):
+    alphabet = string.ascii_lowercase
+    shifted = alphabet[shift:] + alphabet[:shift]
+    table = str.maketrans(alphabet,shifted)
+    decoded = message.translate(table)
+    return (decoded)
 
+def Caear_Shift_en(shift,message):
+    alphabet = string.ascii_lowercase
+    shifted = alphabet[shift:] + alphabet[:shift]
+    table = str.maketrans(shifted,alphabet)
+    encoded = message.translate(table)
+    return (encoded)
+
+#-------------------------------------------------------------------------------------
 while True:
     choicenum = int(input("Welcome to cipher\nWhat do you want to do?\n"
                           "(please chose corresponding number)\n1.Encode\n2.Decode\n3.Quit\n"))
@@ -12,6 +27,7 @@ while True:
                           "(please chose corresponding number)\n1.Caesar Shift\n2.Pigpen\n3.Phonetic Alphabet\n4.Quit\n5.Home\n".format(choice)))
     if ciphernum == 1:
         cipher = "Caesar Shift"
+        shift = int(input("How many shifts would you like?\n"))
     elif ciphernum == 2:
         cipher = "Pigpen"
     elif ciphernum == 3:
@@ -26,8 +42,19 @@ while True:
         break
     elif message == "2":
         continue
-    choice2 = int(input("Your message '{}' was {}d using {}\nNew message:\n...\n(please chose corresponding number)\n1.Quit\n2.Home\n".format(message,choice,cipher)))
+    #----Ciphers------------------------------------------------
+    #----Ceaser Shift-------------------------------------------
+    if choicenum == 1 and ciphernum == 1:
+        new_message =Caear_Shift_en(shift,message)
+    elif choicenum == 2 and ciphernum == 1:
+        new_message = Caear_Shift_de(shift,message)
+
+
+
+    choice2 = int(input("Your message '{}' was {}d using {}\nNew message:\n{}\n"
+                        "(please chose corresponding number)\n1.Quit\n2.Home\n".format(message,choice,cipher,new_message)))
+
     if choice2 == 1:
         break
-    elif choice2 ==2:
+    elif choice2 == 2:
         continue
