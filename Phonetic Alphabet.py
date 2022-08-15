c36d14d750ce5448ee11630b0fda890b8f376161
kemberr <109934204+kemberr@users.noreply.github.com>
2022-08-15 15:19:47 +1200

    Add files via upload

--- Phonetic Alphabet.py
+++ Phonetic Alphabet.py
@@ -5,12 +5,23 @@ alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
                "n","o","p","q","r","s","t","u","v","w","x","y","z"]
 check_value = dict(zip(alphabet, phonetics)) # {'a': 'Alpha', 'b': 'Bravo', 'c': 'Charlie'......
 
-word = input("Please enter your word:\n ")
+word = input("Please enter your word:\n")
 
-for k, v in check_value.items():
-    for letter in word:
-        if letter == k:
-            print(v)
+for alph, phone in check_value.items():
+    numletters = (len(word))
+    for i in range(numletters):
+        if word[i] == alph:
+            print(phone)
+            i +=1
 
+
+
+
+
+#for a, p in check_value.items():
+   # for letter in word:
+      # if letter == a:
+       #     answer = p
+      #      print(answer)
 
 
