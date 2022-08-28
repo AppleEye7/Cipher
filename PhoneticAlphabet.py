f2a49d7e42fb8cfd750655d1c0fccd973b762500
kemberr <109934204+kemberr@users.noreply.github.com>
2022-08-28 14:08:15 +1200

    Add files via upload

--- PhoneticAlphabet.py
+++ PhoneticAlphabet.py
@@ -4,12 +4,13 @@ def Phonetic_Alp_de(message):
                  "sierra","tango","uniform","victor","whiskey","x-ray","yankee","zulu"]
     alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
                 "n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    check_value = dict(zip(phonetics,alphabet )) # {'Alpha': 'a'.....
+    check_value = dict(zip(phonetics,alphabet )) # {'Alpha': 'a', 'Bravo': 'b'... joins both lists into dictionary
     decoded_message = []
-    wordlist = list(message.split(" "))
-    number_of_words = len(wordlist)
+    # /----------------------Creates a list containing each word as a separate object
+    word_list = list(message.split(" "))
+    number_of_words = len(word_list)
     for w in range(0,number_of_words):
-        word = wordlist[w]
+        word = word_list[w]
         alphabet = check_value[word]
         decoded_message.append(alphabet)
     return("".join(decoded_message))
@@ -20,12 +21,12 @@ def Phonetic_Alp_en(message):
                  "Sierra","Tango","Uniform","Victor","Whiskey","X-Ray","Yankee","Zulu"]
     alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
                 "n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    check_value = dict(zip(alphabet, phonetics)) # {'a': 'Alpha', 'b': 'Bravo', 'c': 'Charlie'......
+    check_value = dict(zip(alphabet, phonetics)) # {'a': 'Alpha', 'b': 'Bravo', 'b'...
     encoded_message = []
-    wordlist = list(message.split(" "))
-    number_of_words = len(wordlist)
+    word_list = list(message.split(" "))
+    number_of_words = len(word_list)
     for w in range(0,number_of_words):
-        word = wordlist[w]
+        word = word_list[w]
         number_of_letters = len(word)
         for i in range(0,number_of_letters):
             letter = word[i]
