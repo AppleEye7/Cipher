53057bfa055fba5dbb864c8454af20ba09275654
kemberr <109934204+kemberr@users.noreply.github.com>
2022-08-29 14:34:49 +1200

    Update Phonetic Alphabet.py

--- Phonetic Alphabet.py
+++ Phonetic Alphabet.py
@@ -1,32 +1,36 @@
-phonetics = ["Alpha","Bravo","Charlie","Delta","Echo","Foxtrot","Golf","Hotel","India",
-             "Juliet","Kilo","Lima","Mike","November","Oscar","Papa","Quebec","Romeo",
-             "Sierra","Tango","Uniform","Victor","Whiskey","X-Ray","Yankee","Zulu"]
-alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
-               "n","o","p","q","r","s","t","u","v","w","x","y","z"]
-check_value = dict(zip(alphabet, phonetics)) # {'a': 'Alpha', 'b': 'Bravo', 'c': 'Charlie'......
-newlist = []
-i = 0
-w = 0
-# l == the leter
-# i == index number of letter
-#w == word in wordlist
-inputword = input("Please enter your word:\n").lower()
-wordlist = list(inputword.split(" "))
 
-for alphabet, phonetics in check_value.items():
-    word = wordlist[w]
-    if i == len(word):
-        w += 1
-        i = 0
-    if w == len(wordlist):
-        break
-    for l in alphabet:
-        if l == word[i]:
-            newlist.append(check_value[word[i]])
-        else:
-            newlist.append(check_value[word[i]])
-        i += 1
-    # for second word it repeats second letter twice
+def Phonetic_Alp_de(message):
+    phonetics = ["alpha","bravo","charlie","delta","echo","foxtrot","golf","hotel","india",
+                 "juliet","kilo","lima","mike","november","oscar","papa","quebec","romeo",
+                 "sierra","tango","uniform","victor","whiskey","x-ray","yankee","zulu"]
+    alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
+                "n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    check_value = dict(zip(phonetics,alphabet )) # {'Alpha': 'a', 'Bravo': 'b'... joins both lists into dictionary
+    decoded_message = []
+    # /----------------------Creates a list containing each word as a separate object
+    word_list = list(message.split(" "))
+    number_of_words = len(word_list)
+    for w in range(0,number_of_words):
+        word = word_list[w]
+        alphabet = check_value[word]
+        decoded_message.append(alphabet)
+    return("".join(decoded_message))
 
-print(" ".join(newlist))
-
+def Phonetic_Alp_en(message):
+    phonetics = ["Alpha","Bravo","Charlie","Delta","Echo","Foxtrot","Golf","Hotel","India",
+                 "Juliet","Kilo","Lima","Mike","November","Oscar","Papa","Quebec","Romeo",
+                 "Sierra","Tango","Uniform","Victor","Whiskey","X-Ray","Yankee","Zulu"]
+    alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
+                "n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    check_value = dict(zip(alphabet, phonetics)) # {'a': 'Alpha', 'b': 'Bravo', 'b'...
+    encoded_message = []
+    word_list = list(message.split(" "))
+    number_of_words = len(word_list)
+    for w in range(0,number_of_words):
+        word = word_list[w]
+        number_of_letters = len(word)
+        for i in range(0,number_of_letters):
+            letter = word[i]
+            phonetic = check_value[letter]
+            encoded_message.append(phonetic)
+    return(" ".join(encoded_message))
