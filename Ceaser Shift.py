433a6fb8c8b814857cca3270c79e43a64a95fcb8
kemberr <109934204+kemberr@users.noreply.github.com>
2022-10-02 19:32:54 +1300

    Update Ceaser Shift.py

--- Ceaser Shift.py
+++ Ceaser Shift.py
@@ -1,21 +1,34 @@
-#Caear Shift is a cipher which moves the letters in the message to the right a certain number to encode the message and
-# moves the letter to the left to decode the message
+"""Caear Shift is a cipher which moves the letters position in the message to the right a certain number to encode the
+    message and moves the position to the left a certain number to decode the message"""
+
 import string
 
-def Caear_shift_de(shift,message):
-    alphabet = string.ascii_lowercase  # generates the ascii alphabet in lowercase
-    shifted = alphabet[shift:] + alphabet[:shift]  # moves each letter before shift value(not inclusive) to the end of string
-    table = str.maketrans(alphabet, shifted)  # create dict for alphabet to shifted
-    decoded = message.translate(table)  # message letters translated into the letters from table
-    return decoded
 
-
-def Caear_shift_en(shift,message):
-    alphabet = string.ascii_lowercase  # generates the ascii alphabet in lowercase
-    shifted = alphabet[shift:] + alphabet[:shift]  # moves each letter before shift value(not inclusive) to the end of string
-    table = str.maketrans(shifted, alphabet)  # create dict for shifted to alphabet
-    encoded = message.translate(table)  # message letters translated into the letters from table
+def ceaser_shift_en(shift, message):
+    """Moves each letter in message to the right of their position a certain number which is determined by the value of
+       shift"""
+    # /----------------generates the alphabet in lowercase
+    alphabet = string.ascii_lowercase
+    # /----------------moves each letter before the index of shift value(not inclusive) to the end of string
+    shifted = alphabet[shift:] + alphabet[:shift]
+    # /----------------creates a dictionary for the shifted alphabet to the real alphabet
+    table = str.maketrans(shifted, alphabet)
+    # /-----------------letters from the message changed into the letters from table
+    encoded = message.translate(table)
+    # /-----------------returns the changed message
     return encoded
 
 
-
+def ceaser_shift_de(shift, message):
+    """Moves each letter in message to the left of their position a certain number which is determined by the value of
+       shift"""
+    # /----------------generates the alphabet in lowercase
+    alphabet = string.ascii_lowercase
+    # /----------------moves each letter before the index of shift value(not inclusive) to the end of string
+    shifted = alphabet[shift:] + alphabet[:shift]
+    # /----------------create dictionary for the real alphabet to the shifted alphabet
+    table = str.maketrans(alphabet, shifted)
+    # /-----------------letters from the message changed into the letters from table
+    decoded = message.translate(table)
+    # /-----------------returns the changed message
+    return decoded
